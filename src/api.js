b40be327de059327286d5a8a12ac9bc615db077f
Harshal997 <harshalkher27@gmail.com>
2024-12-04 11:18:16 +0530

    Error handled

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 export const getCountries = async () => {
     try {
-        const response = await fetch('https://xcountries-backend.azurewebsites.net/al');
+        const response = await fetch('https://xcountries-backend.azurewebsites.net/all');
         const data = await response.json();
         console.log("countries",data);
         return data;
